ca23a4a36e82f8366f35b6c51d021200cf53a0a5
Your Name <you@example.com>
2023-08-22 11:29:07 -0700

    3. I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @str: point variable
+ * Return: Void.
+ */
+void _puts(char *str)
+{
+	int indx = 0;
+
+	while (str[indx] != '\0')
+	{
+		_putchar(str[indx]);
+		indx++;
+		str++;
+	}
+	_putchar('\n');
+}
