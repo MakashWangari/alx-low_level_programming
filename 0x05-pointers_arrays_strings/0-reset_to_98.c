88ea96ddd8f5eca3f394f8c69ba0cbbd3ddbb59e
Your Name <you@example.com>
2023-08-22 06:44:14 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter.
+ *@n: dereferenced pointer address
+ * Return: Void.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
