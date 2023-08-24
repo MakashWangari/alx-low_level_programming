aee3b41ae987767bc943fb6bd41a315fe71ca6c0
Your Name <you@example.com>
2023-08-24 12:02:22 -0700

    2. strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strncpy - a function that copies a string
+ * @dest: string copy dest
+ * @src: source copy of string
+ * @n: Maximum no of chars to copy
+ * Return: dest;
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index;
+
+	for (index = 0; src[index] != '\0'; index++)
+	{
+		if (index < n)
+		{
+			dest[index] = src[index];
+		}
+	}
+	for (; index < n; index++)
+	{
+	dest[index] = '\0';
+	}
+	return dest;
+}
