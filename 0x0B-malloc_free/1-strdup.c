324f3d331c23425fdbb085bd973cf99602bb5a9d
Your Name <you@example.com>
2023-09-05 06:11:47 -0700

    1. The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - copies a parameter string
+ * @str: string to be copied
+ *
+ * Return:  pointer to the array.
+ */
+char *_strdup(char *str)
+{
+	int index = 0;
+	int size = 0;
+	char *s;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (size = 0; str[size] != '\0'; size++)
+	{
+	}
+	s = malloc((size + 1) * sizeof(char));
+	while (index < size)
+	{
+		s[index] = str[index];
+		index++;
+	}
+	s[size] = '\0';
+	return (s);
+}
