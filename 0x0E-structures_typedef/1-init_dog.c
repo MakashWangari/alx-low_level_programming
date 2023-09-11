b582b05f577415b066d83bbf1c858201967cc0e6
Your Name <you@example.com>
2023-09-11 12:13:49 -0700

    1. A dog is the only thing on earth that loves you more than you love yourself

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - Function that initialize a variable of type struct dog
+ * @d: pointer to struct dog
+ * @name: Name of dog
+ * @age: Age of dog.
+ * @owner: Owner of dog
+ *
+ * Return: Void.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
