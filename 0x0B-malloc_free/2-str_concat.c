e004aff2a6afb467380132e2e260386f4520ec0c
Makash Wangari <cpamercywangari@gmail.com>
2023-10-03 12:12:30 -0700

    2. He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,48 +1,52 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * str_concat - Function that concantates string
- * @s1: string one
- * @s2: string two
+ * str_concat - concatenates two strings.
+ * @s1: string 1.
+ * @s2: string 2.
  *
- * Return: Pointer to the conc string
+ * Return:  pointer to the array.
  */
 char *str_concat(char *s1, char *s2)
 {
-	int index, indexb, size, a = 0, i = 0;
-	char *combstr;
+	int index, indexb, consize;
+	int size = 0;
+	int length = 0;
+	char *s;
 
-	if (s1 == NULL)
-		i = 0;
-	else
-		while (s1[i] != '\0')
-		{
-			i++;
-		}
-	if (s2 == NULL)
-	{
-		a = 0;
-	}
-	else
-		while (s2[a] != '\0')
-		{
-			a++;
-		}
-	size = a + i;
-	combstr = malloc(sizeof(char) * (size + 1));
-	if (combstr == NULL)
+	if (s1 == NULL && s2 == NULL)
 	{
 		return (NULL);
 	}
-	for (index = 0; index < i; index++)
+	if (s1 != NULL)
 	{
-		combstr[index] = s1[index];
+		for (size = 0; s1[size] != '\0'; size++)
+		{
+		}
 	}
-	for (indexb = 0; indexb < a; index++)
+	if (s2 != NULL)
 	{
-		combstr[i + indexb] = s2[indexb];
+		for (length = 0; s2[length] != '\0'; length++)
+		{
+		}
 	}
-	combstr[a + i] = '\0';
-	return (combstr);
+	consize = size + length;
+	s = malloc((consize + 1) * sizeof(char));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (index = 0; index < size; index++)
+	{
+		s[index] = s1[index];
+	}
+	for (indexb = 0; indexb < length; indexb++)
+	{
+		s[index + indexb] = s2[indexb];
+	}
+
+	s[consize] = '\0';
+	return (s);
 }
